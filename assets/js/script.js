23bd8dcedbcd47962d8aac4e68bfd78505a72348
Nathan Cruz <113571118+crzn24@users.noreply.github.com>
2022-10-30 15:32:30 -0700

    Adds ids to selected city div, writes js code for important dom hooks for html elements

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,8 @@
-// Important DOM elements
+////// Important DOM elements////////
+//selected city elements
+var currentCity = document.getElementById("current-city");
+var temperatureEl = document.getElementById("temperature");
+var windEl = document.getElementById("wind");
+var humidityEl = document.getElementById("humidity");
+
+
