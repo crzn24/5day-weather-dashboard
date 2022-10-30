dd69a1a59abb0ad4264271bf74708492d51f1fa6
Nathan Cruz <113571118+crzn24@users.noreply.github.com>
2022-10-30 15:41:14 -0700

    Added js comments to act as guide/pseudocode

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,3 +6,30 @@ var windEl = document.getElementById("wind");
 var humidityEl = document.getElementById("humidity");
 
 
+
+
+// function to get weather from weather api
+
+
+
+
+// take information given and display it in the dashboard 
+
+
+
+// function to get 5 day forecast
+
+// take info and display it into 5 day forecast
+
+
+
+////// Local Storage /////////
+// get from local storage to show history
+
+
+// save to local storage?
+
+
+
+
+
