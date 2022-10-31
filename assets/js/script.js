7d1704c55365cec43349f89fcafd63f766911e2a
Nathan Cruz <113571118+crzn24@users.noreply.github.com>
2022-10-30 20:21:12 -0700

    Adds var queryURL to js and changed case of APIKey

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,12 +11,14 @@ var windEl = document.getElementById("wind");
 var humidityEl = document.getElementById("humidity");
 
 //API key for weather
-const apiKey = "a8bb7cc0fbe94b1d7ce387b2fe9f1984"
+const APIKey = "a8bb7cc0fbe94b1d7ce387b2fe9f1984"
 
 
 // function to get weather from weather api
 // https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
+// https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
 
+var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + searchCityEl + "&appid=" + APIKey;
 
 // take information given and display it in the dashboard 
 
